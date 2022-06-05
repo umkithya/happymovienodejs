680af29b460c5ff1a2cd16daca958880fc10e84a
umkithya <kithya678@gmail.com>
2022-06-05 21:35:04 +0700

    update

--- app.js
+++ app.js
@@ -1,10 +1,10 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const userRouter = require("./api/users/user.router");
+// const userRouter = require("./api/users/user.router");
 
 
-app.use(express.json());
+// app.use(express.json());
 app.get("/",(req,res)=>{
     console.log("HELLO")
     res.send("HELLO WORLD+++++++++++")
