e2b25b10ae3a9679f6fb06275b2d266ae6ccd40f
umkithya <kithya678@gmail.com>
2022-06-06 20:50:21 +0700

    update

--- auth/token_validation.js
+++ auth/token_validation.js
@@ -3,16 +3,14 @@ const {verify} = require("jsonwebtoken");
 module.exports = {
   checkToken: (req, res, next) => {
     let token = req.get("authorization");
-    console.log("token=====",token);
     if (token) {
       // Remove Bearer from string
       token = token.slice(7);
-      verify(token, process.env.JWT_KEY, (err, decoded) => {
+      verify(token, "qwe1234", (err, decoded) => {
         if (err) {
           return res.json({
             success: 0,
-            message: "Invalid Token...",
-            token: token
+            message: "Invalid Token..."
           });
         } else {
           req.decoded = decoded;
