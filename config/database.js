e9a060a53502135e020c45a11f6398cf6673b96c
umkithya <kithya678@gmail.com>
2022-06-06 15:36:01 +0700

    update

--- config/database.js
+++ config/database.js
@@ -1,9 +1,9 @@
 const {createPool} =require("mysql");
 const pool = createPool({
-    host: "us-cdbr-east-05.cleardb.net",
-    user: "b26e6d391a74a0",
-    password: "94496ae7",
-    database: "heroku_99e3cab4ce08f19",
+    host: "sql101.epizy.com",
+    user: "epiz_31891119",
+    password: "a123456Kk",
+    database: "epiz_31891119_happymovie",
     connectionLimit: 10
 });
 module.exports=pool;
